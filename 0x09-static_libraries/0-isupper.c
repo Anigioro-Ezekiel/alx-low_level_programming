f3741bde637ce3db741e4fe2a085dd013b6119ce
Anigioro-Ezekiel <anigioroea@gmail.com>
2022-10-05 07:46:53 -0700

    task 0

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,24 +1,5 @@
 #include "main.h"
-
-/**
-  * _isupper - Checks for uppercase characters
-  * @c: argument
-  *
-  * Return: 1 if upper, 0 otherwise
-  */
-
-int _isupper(int c)
-{
-	int i;
-	int j;
-
-	i = 'A';
-	j = 'Z';
-
-	if ((c >= i) && (c <= j))
+int _isupper(int c){
 		return (1);
-	else
-		return (0);
 }
-
 
