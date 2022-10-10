93f72d49f40dfe662d15b4ec60cc8139071916dc
Anigioro-Ezekiel <anigioroea@gmail.com>
2022-10-10 11:25:08 -0700

    work

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - a dog's data type
+  * @name: dog name
+  * @age: dog's age
+  * @owner: dog's owner
+  *
+  * Description: this structure models a dog
+  */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif
+
+
